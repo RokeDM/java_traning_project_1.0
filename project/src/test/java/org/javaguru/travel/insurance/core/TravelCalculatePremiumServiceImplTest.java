79d00c8f849a3fc3eba41fc194a1ad00084c0478
RokeDM <dmitri.baumanis@gmail.com>
2025-02-19 10:51:24 +0200

    Step_024_DONE

--- project/src/test/java/org/javaguru/travel/insurance/core/TravelCalculatePremiumServiceImplTest.java
+++ project/src/test/java/org/javaguru/travel/insurance/core/TravelCalculatePremiumServiceImplTest.java
@@ -3,60 +3,58 @@ package org.javaguru.travel.insurance.core;
 import org.javaguru.travel.insurance.rest.TravelCalculatePremiumRequest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Date;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 class TravelCalculatePremiumServiceImplTest {
 
-    private DateTimeService dateTimeService;
+    @Mock private DateTimeService dateTimeService;
+
+    @InjectMocks
     private TravelCalculatePremiumServiceImpl service;
     private TravelCalculatePremiumRequest request;
 
     @BeforeEach
     public void setUP() {
         request = requestAllFields();
-        dateTimeService = mock(DateTimeService.class);
         when(dateTimeService.getDaysBetween(request.getAgreementDateFrom(), request.getAgreementDateTo())).thenReturn(0L);
-        service = new TravelCalculatePremiumServiceImpl(dateTimeService);
     }
 
     @Test
     void testPersonFirstName() {
-        var request = requestAllFields();
         var response = service.calculatePremium(request);
         assertEquals(response.getPersonFirstName(), request.getPersonFirstName());
     }
 
     @Test
     void testPersonLastName() {
-        var request = requestAllFields();
         var response = service.calculatePremium(request);
         assertEquals(response.getPersonLastName(), request.getPersonLastName());
     }
 
     @Test
     void testAgreementDateFrom() {
-        var request = requestAllFields();
         var response = service.calculatePremium(request);
         assertEquals(response.getAgreementDateFrom(), request.getAgreementDateFrom());
     }
 
     @Test
     void testAgreementDateTo() {
-        var request = requestAllFields();
         var response = service.calculatePremium(request);
         assertEquals(response.getAgreementDateTo(), request.getAgreementDateTo());
     }
 
     @Test
     void testAgreementPrice() {
-        var request = requestAllFields();
         var response = service.calculatePremium(request);
         assertNotNull(response.getAgreementPrice());
     }
