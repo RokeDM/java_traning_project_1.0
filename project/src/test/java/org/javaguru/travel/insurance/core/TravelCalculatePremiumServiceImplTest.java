42874703fea4947a5d78d528a1696c021325af48
RokeDM <dmitri.baumanis@gmail.com>
2025-02-25 17:52:25 +0200

    Step_028_DONE

--- project/src/test/java/org/javaguru/travel/insurance/core/TravelCalculatePremiumServiceImplTest.java
+++ project/src/test/java/org/javaguru/travel/insurance/core/TravelCalculatePremiumServiceImplTest.java
@@ -1,6 +1,7 @@
 package org.javaguru.travel.insurance.core;
 
-import org.javaguru.travel.insurance.rest.TravelCalculatePremiumRequest;
+import org.javaguru.travel.insurance.dto.TravelCalculatePremiumRequest;
+import org.javaguru.travel.insurance.dto.ValidationError;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -9,54 +10,116 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Date;
+import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verifyNoInteractions;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class TravelCalculatePremiumServiceImplTest {
 
+    @Mock private TravelCalculatePremiumRequestValidator requestValidator;
     @Mock private DateTimeService dateTimeService;
 
     @InjectMocks
     private TravelCalculatePremiumServiceImpl service;
     private TravelCalculatePremiumRequest request;
 
-    @BeforeEach
-    public void setUP() {
-        request = requestAllFields();
-        when(dateTimeService.getDaysBetween(request.getAgreementDateFrom(), request.getAgreementDateTo())).thenReturn(0L);
-    }
-
     @Test
     void testPersonFirstName() {
+        var request = requestAllFields();
+        when(dateTimeService.getDaysBetween(request.getAgreementDateFrom(), request.getAgreementDateTo())).thenReturn(0L);
+        when(requestValidator.validate(request)).thenReturn(List.of());
         var response = service.calculatePremium(request);
         assertEquals(response.getPersonFirstName(), request.getPersonFirstName());
     }
 
     @Test
     void testPersonLastName() {
+        var request = requestAllFields();
+        when(dateTimeService.getDaysBetween(request.getAgreementDateFrom(), request.getAgreementDateTo())).thenReturn(0L);
+        when(requestValidator.validate(request)).thenReturn(List.of());
         var response = service.calculatePremium(request);
         assertEquals(response.getPersonLastName(), request.getPersonLastName());
     }
 
     @Test
     void testAgreementDateFrom() {
+        var request = requestAllFields();
+        when(dateTimeService.getDaysBetween(request.getAgreementDateFrom(), request.getAgreementDateTo())).thenReturn(0L);
+        when(requestValidator.validate(request)).thenReturn(List.of());
         var response = service.calculatePremium(request);
         assertEquals(response.getAgreementDateFrom(), request.getAgreementDateFrom());
     }
 
     @Test
     void testAgreementDateTo() {
+        var request = requestAllFields();
+        when(dateTimeService.getDaysBetween(request.getAgreementDateFrom(), request.getAgreementDateTo())).thenReturn(0L);
+        when(requestValidator.validate(request)).thenReturn(List.of());
         var response = service.calculatePremium(request);
         assertEquals(response.getAgreementDateTo(), request.getAgreementDateTo());
     }
 
     @Test
     void testAgreementPrice() {
+        var request = requestAllFields();
+        when(dateTimeService.getDaysBetween(request.getAgreementDateFrom(), request.getAgreementDateTo())).thenReturn(0L);
+        when(requestValidator.validate(request)).thenReturn(List.of());
         var response = service.calculatePremium(request);
         assertNotNull(response.getAgreementPrice());
+    }
+
+    @Test
+    void testReturnResponseWithErrors() {
+        var request = new TravelCalculatePremiumRequest();
+        var validationError = new ValidationError("field", "message");
+        when(requestValidator.validate(request)).thenReturn(List.of(validationError));
+        var response = service.calculatePremium(request);
+        assertTrue(response.hasErrors());
+    }
+
+    @Test
+    void testReturnResponseWithCorrectErrorCount() {
+        var request = new TravelCalculatePremiumRequest();
+        var validationError = new ValidationError("field", "message");
+        when(requestValidator.validate(request)).thenReturn(List.of(validationError));
+        var response = service.calculatePremium(request);
+        assertEquals(response.getErrors().size(), 1);
+    }
+
+    @Test
+    void testReturnResponseWithCorrectError() {
+        var request = new TravelCalculatePremiumRequest();
+        var validationError = new ValidationError("field", "message");
+        when(requestValidator.validate(request)).thenReturn(List.of(validationError));
+        var response = service.calculatePremium(request);
+        assertEquals(response.getErrors().get(0).getField(), "field");
+        assertEquals(response.getErrors().get(0).getMessage(), "message");
+        assertNull(response.getPersonFirstName());
+    }
+
+    @Test
+    void testFieldsMustBeEmptyIfContainsErrors() {
+        var request = new TravelCalculatePremiumRequest();
+        var validationError = new ValidationError("field", "message");
+        when(requestValidator.validate(request)).thenReturn(List.of(validationError));
+        var response = service.calculatePremium(request);
+        assertNull(response.getPersonFirstName());
+        assertNull(response.getPersonLastName());
+        assertNull(response.getAgreementDateFrom());
+        assertNull(response.getAgreementDateTo());
+        assertNull(response.getAgreementPrice());
+    }
+
+    @Test
+    void testNOtBeInteractionWithDateTimeServiceWhenResponseContainsError() {
+        var request = new TravelCalculatePremiumRequest();
+        var validationError = new ValidationError("field", "message");
+        when(requestValidator.validate(request)).thenReturn(List.of(validationError));
+        var response = service.calculatePremium(request);
+        verifyNoInteractions(dateTimeService);
     }
 
     private TravelCalculatePremiumRequest requestAllFields() {
