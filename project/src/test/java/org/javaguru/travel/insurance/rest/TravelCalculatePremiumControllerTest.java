dfcabf86575753dfd08a21c6834fe4f11cef3bf6
RokeDM <dmitri.baumanis@gmail.com>
2025-03-17 22:05:41 +0200

    Step_048_DONE

--- project/src/test/java/org/javaguru/travel/insurance/rest/TravelCalculatePremiumControllerTest.java
+++ project/src/test/java/org/javaguru/travel/insurance/rest/TravelCalculatePremiumControllerTest.java
@@ -1,5 +1,6 @@
 package org.javaguru.travel.insurance.rest;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,7 +11,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -23,21 +24,165 @@ public class TravelCalculatePremiumControllerTest {
     @Autowired private MockMvc mockMvc;
 
     @Test
-    public void simpleRestControllerTest() throws Exception {
+    @DisplayName("Test case 1: person first name is null")
+    public void personFirstNameNullTest() throws Exception {
         mockMvc.perform(post("/insurance/travel/")
                         .content("{" +
-                                "\"personFirstName\" : \"Vasja\",\n" +
+                                "\"personFirstName\" : null,\n" +
                                 "\"personLastName\" : \"Pupkin\",\n" +
                                 "\"agreementDateFrom\" : \"2021-05-25\",\n" +
                                 "\"agreementDateTo\" : \"2021-05-29\"\n" +
                                 "}")
                         .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE))
                 .andExpect(status().isOk())
+                .andExpect(jsonPath("personFirstName", is(nullValue())))
+                .andExpect(jsonPath("personLastName", is(nullValue())))
+                .andExpect(jsonPath("agreementDateFrom", is(nullValue())))
+                .andExpect(jsonPath("agreementDateTo", is(nullValue())))
+                .andExpect(jsonPath("agreementPrice", is(nullValue())))
+                .andExpect(jsonPath("errors", is(notNullValue())))
+                .andExpect(jsonPath("errors", hasSize(1)))
+                .andExpect(jsonPath("errors[0].field", is("personFirstName")))
+                .andExpect(jsonPath("errors[0].message", is("Must not be empty!")))
+                .andReturn();
+    }
+
+    @Test
+    @DisplayName("Test case 2: person last name is null")
+    public void personLastNameNullTest() throws Exception {
+        mockMvc.perform(post("/insurance/travel/")
+                        .content("{" +
+                                "\"personFirstName\" : \"Vasja\",\n" +
+                                "\"personLastName\" : null,\n" +
+                                "\"agreementDateFrom\" : \"2021-05-25\",\n" +
+                                "\"agreementDateTo\" : \"2021-05-29\"\n" +
+                                "}")
+                        .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("personFirstName", is(nullValue())))
+                .andExpect(jsonPath("personLastName", is(nullValue())))
+                .andExpect(jsonPath("agreementDateFrom", is(nullValue())))
+                .andExpect(jsonPath("agreementDateTo", is(nullValue())))
+                .andExpect(jsonPath("agreementPrice", is(nullValue())))
+                .andExpect(jsonPath("errors", is(notNullValue())))
+                .andExpect(jsonPath("errors", hasSize(1)))
+                .andExpect(jsonPath("errors[0].field", is("personLastName")))
+                .andExpect(jsonPath("errors[0].message", is("Must not be empty!")))
+                .andReturn();
+    }
+
+    @Test
+    @DisplayName("Test case 3: agreement date from is null")
+    public void agreementDateFromNullTest() throws Exception {
+        mockMvc.perform(post("/insurance/travel/")
+                        .content("{" +
+                                "\"personFirstName\" : \"Vasja\",\n" +
+                                "\"personLastName\" : \"Pupkin\",\n" +
+                                "\"agreementDateFrom\" : null,\n" +
+                                "\"agreementDateTo\" : \"2021-05-29\"\n" +
+                                "}")
+                        .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("personFirstName", is(nullValue())))
+                .andExpect(jsonPath("personLastName", is(nullValue())))
+                .andExpect(jsonPath("agreementDateFrom", is(nullValue())))
+                .andExpect(jsonPath("agreementDateTo", is(nullValue())))
+                .andExpect(jsonPath("agreementPrice", is(nullValue())))
+                .andExpect(jsonPath("errors", is(notNullValue())))
+                .andExpect(jsonPath("errors", hasSize(1)))
+                .andExpect(jsonPath("errors[0].field", is("agreementDateFrom")))
+                .andExpect(jsonPath("errors[0].message", is("Must not be empty!")))
+                .andReturn();
+    }
+
+    @Test
+    @DisplayName("Test case 4: agreement date to is null")
+    public void agreementDateToNullTest() throws Exception {
+        mockMvc.perform(post("/insurance/travel/")
+                        .content("{" +
+                                "\"personFirstName\" : \"Vasja\",\n" +
+                                "\"personLastName\" : \"Pupkin\",\n" +
+                                "\"agreementDateFrom\" : \"2021-05-21\",\n" +
+                                "\"agreementDateTo\" : null\n" +
+                                "}")
+                        .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("personFirstName", is(nullValue())))
+                .andExpect(jsonPath("personLastName", is(nullValue())))
+                .andExpect(jsonPath("agreementDateFrom", is(nullValue())))
+                .andExpect(jsonPath("agreementDateTo", is(nullValue())))
+                .andExpect(jsonPath("agreementPrice", is(nullValue())))
+                .andExpect(jsonPath("errors", is(notNullValue())))
+                .andExpect(jsonPath("errors", hasSize(1)))
+                .andExpect(jsonPath("errors[0].field", is("agreementDateTo")))
+                .andExpect(jsonPath("errors[0].message", is("Must not be empty!")))
+                .andReturn();
+    }
+
+    @Test
+    @DisplayName("Test case 5: all fields are null")
+    public void allFieldsNullTest() throws Exception {
+        mockMvc.perform(post("/insurance/travel/")
+                        .content("{" +
+                                "\"personFirstName\" : null,\n" +
+                                "\"personLastName\" : null,\n" +
+                                "\"agreementDateFrom\" : null,\n" +
+                                "\"agreementDateTo\" : null\n" +
+                                "}")
+                        .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("personFirstName", is(nullValue())))
+                .andExpect(jsonPath("personLastName", is(nullValue())))
+                .andExpect(jsonPath("agreementDateFrom", is(nullValue())))
+                .andExpect(jsonPath("agreementDateTo", is(nullValue())))
+                .andExpect(jsonPath("agreementPrice", is(nullValue())))
+                .andExpect(jsonPath("errors", is(notNullValue())))
+                .andExpect(jsonPath("errors", hasSize(4)))
+                .andReturn();
+    }
+
+    @Test
+    @DisplayName("Test case 6: agreement date to less than agreement date from")
+    public void agreementDateToLessThanDateFromTest() throws Exception {
+        mockMvc.perform(post("/insurance/travel/")
+                        .content("{" +
+                                "\"personFirstName\" : \"Vasja\",\n" +
+                                "\"personLastName\" : \"Pupkin\",\n" +
+                                "\"agreementDateFrom\" : \"2021-05-29\",\n" +
+                                "\"agreementDateTo\" : \"2021-05-21\"\n" +
+                                "}")
+                        .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("personFirstName", is(nullValue())))
+                .andExpect(jsonPath("personLastName", is(nullValue())))
+                .andExpect(jsonPath("agreementDateFrom", is(nullValue())))
+                .andExpect(jsonPath("agreementDateTo", is(nullValue())))
+                .andExpect(jsonPath("agreementPrice", is(nullValue())))
+                .andExpect(jsonPath("errors", is(notNullValue())))
+                .andExpect(jsonPath("errors", hasSize(1)))
+                .andExpect(jsonPath("errors[0].field", is("agreementDateFrom")))
+                .andExpect(jsonPath("errors[0].message", is("Must be less then agreementDateTo!")))
+                .andReturn();
+    }
+
+    @Test
+    @DisplayName("Test case 7: all fields are OK")
+    public void allFieldsOkTest() throws Exception {
+        mockMvc.perform(post("/insurance/travel/")
+                        .content("{" +
+                                "\"personFirstName\" : \"Vasja\",\n" +
+                                "\"personLastName\" : \"Pupkin\",\n" +
+                                "\"agreementDateFrom\" : \"2021-05-21\",\n" +
+                                "\"agreementDateTo\" : \"2021-05-29\"\n" +
+                                "}")
+                        .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(status().isOk())
                 .andExpect(jsonPath("personFirstName", is("Vasja")))
                 .andExpect(jsonPath("personLastName", is("Pupkin")))
-                .andExpect(jsonPath("agreementDateFrom", is("2021-05-25")))
+                .andExpect(jsonPath("agreementDateFrom", is("2021-05-21")))
                 .andExpect(jsonPath("agreementDateTo", is("2021-05-29")))
-                .andExpect(jsonPath("agreementPrice", is(4)))
+                .andExpect(jsonPath("agreementPrice", is(8)))
+                .andExpect(jsonPath("errors", is(nullValue())))
                 .andReturn();
     }
 }
